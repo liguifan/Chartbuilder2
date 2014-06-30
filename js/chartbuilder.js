0429f880df0e3b06e70e0cb296a14f10d2358db2
Bread <yangsg1026@gmail.com>
2014-06-30 00:52:36 -0400

    recover

--- js/chartbuilder.js
+++ js/chartbuilder.js
@@ -309,12 +309,13 @@ ChartBuilder = {
 
         var svg = $.trim(document.getElementById("chartContainer").innerHTML);
         var svg1 = $.trim(document.getElementById("chartContainer").innerHTML);
-        var logo = document.getElementById("ALMlogo").cloneNode(true);
+        var logoraw = document.getElementById("ALMlogo");
+        var logo =  logoraw.cloneNode(true);
         logo.style.width = '200%'
         logo.style.width = '200%'
         canvasContext.drawSvg(svg,0,0);
         var height = $("#chartContainer").height()* 2
-        canvasContext.drawImg(logo,0,height);
+        canvasContext.drawImage(logo,0,height);
 
 		
 		var filename = [];
