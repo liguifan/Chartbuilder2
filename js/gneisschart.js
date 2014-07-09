1b208109e74e54a60a3486f13c3fa9b4372d9dec
Bread <yangsg1026@gmail.com>
2014-07-09 02:54:58 -0400

    fuck

--- js/gneisschart.js
+++ js/gneisschart.js
@@ -1822,7 +1822,7 @@ function Gneiss(config)
                                 function(d,i) {return g.xAxis().scale(g.xAxisRef()[0].data[i])}:
                                 function(d,i) {return g.xAxis().scale(i)}
                                 )
-                          .attr("y", function(d,i) {yAxisIndex = d3.select(this.parentNode).data()[0].axis; return d || d ===0 ? (g.yAxis()[yAxisIndex]+3).scale(d) : -100})
+                          .attr("y", function(d,i) {yAxisIndex = d3.select(this.parentNode).data()[0].axis; return d || d ===0 ? (g.yAxis()[yAxisIndex]).scale(d) : -100})
                           
                           
                           yAxisIndex = d3.select(this.parentNode).data()[0].axis; return Math.abs(g.yAxis()[yAxisIndex].scale(d) - g.yAxis()[yAxisIndex].scale(Gneiss.helper.columnXandHeight(d,g.yAxis()[yAxisIndex].scale.domain())))})
@@ -1845,8 +1845,8 @@ function Gneiss(config)
  							function(d,i) {return g.xAxis().scale(g.xAxisRef()[0].data[i])}:
  							function(d,i) {return g.xAxis().scale(i)}
  					)
-                    .attr("y", function(d,i) {yAxisIndex = d3.select(this.parentNode).data()[0].axis; return d || d ===0 ? (g.yAxis()[yAxisIndex]+3).scale(d) : -100})
-                
+ 					.attr("y", function(d,i) {yAxisIndex = d3.select(this.parentNode).data()[0].axis; return d || d ===0 ? g.yAxis()[yAxisIndex].scale(d) : -100})
+ 
 
 				//add lines
 				lineSeries = g.seriesContainer.selectAll("path")
